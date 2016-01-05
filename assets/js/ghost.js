2d00f6538e4b019722a462e774ed4979a891874c
Herobs <herobs@herobs.cn>
2016-01-05 11:22:27 +0800

    add code highlight

--- assets/js/ghost.js
+++ assets/js/ghost.js
@@ -1,28 +1,30 @@
 $(function() {
     var currentYPosition = function() {
-        if (window.pageYOffset)
+        if (window.pageYOffset) {
             return window.pageYOffset;
-        else if (window.document.documentElement && window.document.documentElement.scrollTop)
+        } else if (window.document.documentElement && window.document.documentElement.scrollTop) {
             return window.document.documentElement.scrollTop;
-        else if (window.document.body.scrollTop)
+        } else if (window.document.body.scrollTop) {
             return window.document.body.scrollTop;
-        else
+        } else {
             return 0;
+        }
     }
 
     var scrollToTop = $('#scroll-to-top');
     $(window).on('scroll', function() {
-        if (currentYPosition() > 96)
+        if (currentYPosition() > 96) {
             scrollToTop.fadeIn();
-        else
+        } else {
             scrollToTop.fadeOut();
+        }
     });
     scrollToTop.click(function() {
         $("html, body").animate({ scrollTop: 0 }, 300);
     });
 
     $externalLinkIcon = $('<i class="post__external-link fa fa-external-link"></i>');
-    $.each($('.post__body a'), function() {
+    $.each($('.post__content a'), function() {
         var a = $(this);
         a.attr('target', '_blank');
         $externalLinkIcon.clone().appendTo(a);
